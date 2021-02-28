18625484cb01aeeb818ae9e1bd9f0b7bb8949d96
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2021-02-27 21:01:38 -0300

    Adicionando kit do font awesome

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,31 +9,34 @@ class Layout extends Component {
   render() {
     const { children } = this.props;
     return (
-      <StaticQuery
-        query={graphql`
-          query SiteTitleQuery {
-            site {
-              siteMetadata {
-                title
+      <React.Fragment>
+        <StaticQuery
+          query={graphql`
+            query SiteTitleQuery {
+              site {
+                siteMetadata {
+                  title
+                }
               }
             }
-          }
-        `}
-        render={data => (
-          <>
-            <Helmet
-              title={data.site.siteMetadata.title}
-              meta={[
-                { name: 'description', content: 'Resume' },
-                { name: 'keywords', content: 'site, web' },
-              ]}
-            >
-              <html lang="en" />
-            </Helmet>
-            <div className={'main-body'}>{children}</div>
-          </>
-        )}
-      />
+          `}
+          render={data => (
+            <>
+              <Helmet
+                title={data.site.siteMetadata.title}
+                meta={[
+                  { name: 'description', content: 'Resume' },
+                  { name: 'keywords', content: 'site, web' },
+                ]}
+              >
+                <html lang="en" />
+              </Helmet>
+              <div className={'main-body'}>{children}</div>
+            </>
+          )}
+        />
+        <script src="https://kit.fontawesome.com/ddd17c865d.js" crossorigin="anonymous"></script>
+      </React.Fragment>
     );
   }
 }
